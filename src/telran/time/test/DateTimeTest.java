bf09d70738710eff8d37e71e0c9c3d57d8b1fa93
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-05-02 20:16:26 +0300

    CW 18 difference between temporal's

--- src/telran/time/test/DateTimeTest.java
+++ src/telran/time/test/DateTimeTest.java
@@ -3,7 +3,9 @@ package telran.time.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
@@ -25,5 +27,20 @@ class DateTimeTest {
 				birthDateAS.format(DateTimeFormatter.ofPattern("MMMM,d.yyyy",Locale.forLanguageTag("he"))));
 		System.out.printf("birthdate of ASP in dd/mm/yyyy format is %s\n", 
 				birthDateAS.format(DateTimeFormatter.ofPattern("MMMM,dd.yyyy EEEE",Locale.forLanguageTag("ru"))));
+	}
+	@Test
+	void introductionTest1() {
+		LocalDate d1 = LocalDate.parse("1990-01-30");
+		LocalDate d2= LocalDate.parse("2000-10-20");
+//		ChronoUnit unit = ChronoUnit.MONTHS;
+		ChronoUnit unit = ChronoUnit.DAYS;
+		System.out.printf("difference between %s and %s in %s is %d\n",d1,d2,unit,unit.between(d1, d2));
+	}
+	@Test
+	void introductionTest2() {
+		LocalDateTime d1 = LocalDateTime.parse("1990-01-30T00:00:00");
+		LocalDateTime d2= LocalDateTime.parse("2000-10-20T00:00:00");
+		ChronoUnit unit = ChronoUnit.SECONDS;
+		System.out.printf("difference between %s and %s in %s is %d\n",d1,d2,unit,unit.between(d1, d2));
 	}
 }
